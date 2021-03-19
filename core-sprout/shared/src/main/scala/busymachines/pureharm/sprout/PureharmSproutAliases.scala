2beafbe44b71a116cdcc0628b9fb68d267269cc0
Loránd Szakács <lorand.szakacs@protonmail.com>
2021-03-19 13:45:37 +0200

    Move traits into their respective files

--- core-sprout/shared/src/main/scala/busymachines/pureharm/sprout/PureharmSproutAliases.scala
+++ core-sprout/shared/src/main/scala/busymachines/pureharm/sprout/PureharmSproutAliases.scala
@@ -16,12 +16,24 @@
 
 package busymachines.pureharm.sprout
 
-trait PureharmSproutImplicits {
-  import cats.Show
-  import cats.implicits._
+trait PureharmSproutAliases {
+  type Sprout[O]                = _root_.sprout.Sprout[O]
+  type SproutSub[O]             = _root_.sprout.SproutSub[O]
+  type SproutRefined[O, E]      = _root_.sprout.SproutRefined[O, E]
+  type SproutRefinedSub[O, E]   = _root_.sprout.SproutRefinedSub[O, E]
+  type SproutRefinedThrow[O]    = _root_.sprout.SproutRefinedThrow[O]
+  type SproutRefinedSubThrow[O] = _root_.sprout.SproutRefinedSub[O, Throwable]
 
-  implicit final def pureharmSproutShowGeneric[Old, New](implicit
-    show:    Show[Old],
-    oldType: OldType[Old, New],
-  ): Show[New] = show.contramap(oldType.oldType)
+  type OldType[O, N] = _root_.sprout.OldType[O, N]
+  val OldType: _root_.sprout.OldType.type = _root_.sprout.OldType
+  type NewType[O, N] = _root_.sprout.NewType[O, N]
+  val NewType: _root_.sprout.NewType.type = _root_.sprout.NewType
+  type RefinedType[O, N, E] = _root_.sprout.RefinedType[O, N, E]
+  val RefinedType: _root_.sprout.RefinedType.type = _root_.sprout.RefinedType
+  type RefinedTypeThrow[O, N] = _root_.sprout.RefinedTypeThrow[O, N]
+  val RefinedTypeThrow: _root_.sprout.RefinedTypeThrow.type = _root_.sprout.RefinedTypeThrow
+
+  type SproutShow[O]  = _root_.sprout.SproutShow[O]
+  type SproutEq[O]    = _root_.sprout.SproutEq[O]
+  type SproutOrder[O] = _root_.sprout.SproutOrder[O]
 }
